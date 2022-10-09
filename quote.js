27b9dbff72d774d6df08b271320ee22fb5b3461a
asimdm <asimalidm.2020cs@technonjr.org>
2022-10-10 00:15:04 +0530

    update

--- quote.js
+++ quote.js
@@ -1,21 +1,15 @@
-let quote=[
-    "\"If you want to live a happy life, tie it to a goal, not to people or things.\"",
-    "\"Never let the fear of striking out keep you from playing the game.\"",
-    "\"Money and success don\'t change people; they merely amplify what is already there.\"",
-    "\"Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma - which is living with the results of other people\'s thinking.\"",
-    "\"If life were predictable it would cease to be life, and be without flavor.\""
-]
+let quote=document.getElementById("Quote");
+let author=document.getElementById("author");
+let btn=document.getElementById("btn");
 
-let author=[
-    "- Albert Einstein",
-    "- Babe Ruth",
-    "- Will Smith",
-    "- Steve Jobs",
-    "- Eleanor Roosevelt"
-]
+const api="https://free-quotes-api.herokuapp.com/";
 
-function next(){
-    let x=Math.floor((Math.random()*5));
-    document.getElementById("Quote").innerHTML=quote[x];
-    document.getElementById("author").innerHTML=author[x];
-}
+let getQuote =() => {
+    fetch(api).then((data_received) => data_received.json()).then((item) =>{
+        quote.innerText=item.quote;
+        author.innerText=item.author;
+    });
+};
+
+window.addEventListener("load",getQuote);
+btn.addEventListener("click",getQuote);
